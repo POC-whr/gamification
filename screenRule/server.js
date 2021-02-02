28b01eee19d231b29b1036f5383f1a23d27558ca
Joselma05 <joselma@outlook.com>
2021-02-02 22:44:28 +0000

    update code

--- screenRule/server.js
+++ screenRule/server.js
@@ -8,7 +8,7 @@ var port  = process.env.PORT || 3000;
 var options = {
 	anonymous : true, // remove to authenticate calls
 	auditLog : { logToConsole: true }, // change to auditlog service for productive scenarios
-	redirectUrl : "/index.xsjs"
+	redirectUrl : "/matchCode/listas.xsodata/seller?$format=json"
 };
 
 // configure HANA
